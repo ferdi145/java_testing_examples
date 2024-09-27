bf6a689e839e7f98724ee91536b0bc9bbb89d69e
Ferdinand Ade <ferdinand.ade@otto.de>
2024-09-27 10:18:45 +0200

    disable api call test

--- src/test/java/examples/apifetch/directlyfromapi/FetchFromApi.java
+++ src/test/java/examples/apifetch/directlyfromapi/FetchFromApi.java
@@ -1,11 +1,13 @@
 package examples.apifetch.directlyfromapi;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 
 class FetchFromApi {
 
+    @Disabled("long running because of real API call")
     @Test
     void fetchFromApi() {
         APeopleApiService peopleApiService = new APeopleApiService();
